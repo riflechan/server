637e62c2360457cbf49e6e2d0f29198803f9f21f
Vladislav Vaintroub <wlad@mariadb.com>
2017-10-29 10:23:24 +0000

    temporarily increase all timeouts to catch SSL hang live

--- mysql-test/collections/buildbot_suites.bat
+++ mysql-test/collections/buildbot_suites.bat
@@ -1,4 +1,4 @@
-perl mysql-test-run.pl --verbose-restart --force --testcase-timeout=45 --suite-timeout=600 --max-test-fail=500 --retry=3  --parallel=4 --suite=^
+perl mysql-test-run.pl --verbose-restart --force --testcase-timeout=450000 --suite-timeout=600000 --max-test-fail=500 --retry=3  --parallel=4 --suite=^
 main,^
 innodb,^
 plugins,^
